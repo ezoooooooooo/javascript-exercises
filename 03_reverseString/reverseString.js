989f00e68cf53a38ed48d93a082bef553009bece
ezoooooooooo <ezooahmed39@gmail.com>
2024-08-30 17:17:12 +0300

    Solve third exercise

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(word) {
+   let x =word.length;
+   let reversed = '';
+   for(let i = x-1;i>= 0;i--){
+    reversed +=word.charAt(i);
+   } 
+    return reversed;
 };
 
 // Do not edit below this line
