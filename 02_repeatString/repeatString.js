7a9b07fbf3dc8fe67acf95ac64f3396c075f22ed
ezoooooooooo <ezooahmed39@gmail.com>
2024-08-30 15:50:17 +0300

    Solve second exercise

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,4 +1,14 @@
-const repeatString = function() {
+const repeatString = function(word,num) {
+  let result = '';
+  if(num < 0){
+   result = "ERROR"
+  }
+  else{for(let i = 1; i<=num;i++){
+     console.log(word);
+     result +=word;
+   }}
+   
+    return result;
 
 };
 
