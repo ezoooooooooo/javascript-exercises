8f4fd509b4a6c0d0de7f9fc09bf6b5e8967f9c33
ezoooooooooo <ezooahmed39@gmail.com>
2024-09-03 07:48:38 +0300

    Solve exercies 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(x) {
+    let C = (x -32)*5/9;
+    C = Math.round(C *10)/10;
+    return C;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(x) {
+  let F = (x * 9/5) + 32
+  F = Math.round(F*10)/10;
+  return F;
 };
 
 // Do not edit below this line
