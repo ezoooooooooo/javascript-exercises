b4c8979f690f1e8a2f0d20f4150f5aaca59ed697
ezoooooooooo <ezooahmed39@gmail.com>
2024-09-03 07:37:30 +0300

    Solve exercise 6

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,17 @@
-const leapYears = function() {
-
+const leapYears = function(x) {
+ if(x%4==0 && x%100!=0 )
+ {
+  console.log(x+" is a leap year")
+  return true;
+ }
+ else if (x%4==0 && x%100==0 && x%400==0){
+  console.log(x+" is a leap year")
+  return true;
+ }
+ else{
+  console.log(x+ " is not a leap year")
+  return false;
+ }
 };
 
 // Do not edit below this line
