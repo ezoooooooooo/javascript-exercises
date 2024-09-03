5d07afcb4c3dc3b0b7c2dab1ccf4f965c8f5f38a
ezoooooooooo <ezooahmed39@gmail.com>
2024-09-03 07:22:04 +0300

    Solve exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,20 @@
-const sumAll = function() {
-
+const sumAll = function (x, y) {
+  let sum = 0;
+  if (x === parseInt(x) && y === parseInt(y) && x >0 && y >0){if(x<y){for (let i = x; i <= y; i++) {
+    sum += i;
+  }
+  return sum;}
+  else{
+    for(let i = y; i<=x;i++){
+      sum +=i;
+    }
+    return sum;
+  }}
+  else{
+    return "ERROR"
+  }
+  
+  
 };
 
 // Do not edit below this line
